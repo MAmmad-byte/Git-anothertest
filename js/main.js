b8e17f49bd57acfcffe016ee4fea8c8759e32a63
MAmmad-byte <muhammadammad146@gmail.com>
2021-04-23 01:58:29 +0500

    test repoditory

---
+++ js/main.js
@@ -0,0 +1,85 @@
+TweenMax.fromTo(".bread-top", {x:"-50%"},
+{
+  scrollTrigger: {
+    trigger: ".ingredients-img",
+    start: "top 55%",
+    end: "bottom 45%",
+    toggleActions: "restart complete none reset"
+  },
+  x: "-50%",
+   y: -250});
+
+
+   TweenMax.fromTo(".cheese-top", {x:"-50%"},
+   {
+     scrollTrigger: {
+       trigger: ".ingredients-img",
+       start: "top 55%",
+       end: "bottom 45%",
+       toggleActions: "restart complete none reset"
+     },
+     x: "-50%",
+      y: -70});
+
+
+      TweenMax.fromTo(".onion", {x:"-50%"},
+      {
+        scrollTrigger: {
+          trigger: ".ingredients-img",
+          start: "top 55%",
+          end: "bottom 45%",
+          toggleActions: "restart complete none reset"
+        },
+        x: "-50%",
+         y: 50});
+
+         TweenMax.fromTo(".tomato", {x:"-50%"},
+         {
+           scrollTrigger: {
+             trigger: ".ingredients-img",
+             start: "top 55%",
+             end: "bottom 45%",
+             toggleActions: "restart complete none reset"
+           },
+           x: "-50%",
+            y: 100});
+
+
+            TweenMax.fromTo(".cucumber", {x:"-50%"},
+            {
+              scrollTrigger: {
+                trigger: ".ingredients-img",
+                start: "top 55%",
+                end: "bottom 45%",
+                toggleActions: "restart complete none reset"
+              },
+              x: "-50%",
+               y: 200});
+
+
+
+               TweenMax.fromTo(".cheese-bottom", {x:"-50%"},
+               {
+                 scrollTrigger: {
+                   trigger: ".ingredients-img",
+                   start: "top 55%",
+                   end: "bottom 45%",
+                   toggleActions: "restart complete none reset"
+                 },
+                 x: "-50%",
+                  y: 200});
+
+
+                  TweenMax.fromTo(".bread-bottom", {x:"-50%"},
+                  {
+                    scrollTrigger: {
+                      trigger: ".ingredients-img",
+                      start: "top 55%",
+                      end: "bottom 45%",
+                      toggleActions: "restart complete none reset"
+                    },
+                    x: "-50%",
+                     y: 350});
+
+                
+
